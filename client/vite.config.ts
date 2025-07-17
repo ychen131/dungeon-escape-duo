42218e157432b2aa13b7bf5b4241c0030d210931
yiran-gauntlet <chuanchuanc@gmail.com>
2025-07-16 23:38:51 -0500

    auto build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    watch: {
+      // Watch mode options
+      include: ['src/**'],
+    },
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   server: {
     proxy: {
       '/socket.io': {
