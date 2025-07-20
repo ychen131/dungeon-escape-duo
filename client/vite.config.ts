731e21dbe58d858a1ae960d788ba201a04256f83
yiran-gauntlet <chuanchuanc@gmail.com>
2025-07-20 18:33:32 -0500

    cloudflare deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  plugins: [cloudflare()],
   build: {
     outDir: 'dist',
     watch: {
